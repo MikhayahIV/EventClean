5e5a2ebc5ad9ad8de804f1bd2c4603d6468cabc3
Sasha <sasha.vessa@gmail.com>
2025-09-26 21:04:08 -0300

    refactor: remove as anotations do lombok e adicionar os Construtores,getters e setters

--- src/main/java/six/jay/EventClean/infrastructure/persistence/EventEntity.java
+++ src/main/java/six/jay/EventClean/infrastructure/persistence/EventEntity.java
@@ -1,16 +1,10 @@
 package six.jay.EventClean.infrastructure.persistence;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import six.jay.EventClean.core.enums.EventTypes;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name="tb_events")
 public class EventEntity {
@@ -25,17 +19,117 @@ public class EventEntity {
 
     private String identificador;
 
+    @Column(name = "data_inicio")
     private LocalDateTime dataInicio;
 
+    @Column(name = "data_fim")
     private LocalDateTime dataFim;
 
+    @Column(name = "local_evento")
     private String localEvento;
 
     private Integer capacidade;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_evento")
     private EventTypes tipo;
 
     private String organizador;
 
+
+    public EventEntity() {
+    }
+
+    public EventEntity(Long id, String nome, String descricao, String identificador, LocalDateTime dataInicio, LocalDateTime dataFim, String localEvento, Integer capacidade, EventTypes tipo, String organizador) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.identificador = identificador;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.localEvento = localEvento;
+        this.capacidade = capacidade;
+        this.tipo = tipo;
+        this.organizador = organizador;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public LocalDateTime getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDateTime dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDateTime getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDateTime dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getLocalEvento() {
+        return localEvento;
+    }
+
+    public void setLocalEvento(String localEvento) {
+        this.localEvento = localEvento;
+    }
+
+    public Integer getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public EventTypes getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(EventTypes tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getOrganizador() {
+        return organizador;
+    }
+
+    public void setOrganizador(String organizador) {
+        this.organizador = organizador;
+    }
 }
